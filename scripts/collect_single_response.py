17d9a974f54f1818e3c272644cac56c23a526446
guest400123064 <lgfz1003@icloud.com>
2023-02-23 02:28:20 -0500

    test statistics

--- scripts/collect_single_response.py
+++ scripts/collect_single_response.py
@@ -7,7 +7,7 @@
     different hyperparameter configurations. The 'therapist' will ask 
     a single question and the 'patient' will give a single response."""
 
-from typing import Callable, Dict, List, Any
+from typing import Callable, Dict, List, Any, Tuple, Sequence
 import copy
 import tqdm
 import json
@@ -28,42 +28,43 @@ Patient:
 """.strip()
 
 
-def get_questions(path: str = "") -> List[str]:
+def get_questions(path: str = "") -> Sequence[Tuple[int, str]]:
     """Return a set of questions for the simulated patient."""
     
-    return ["How would you feel if someone called you a jerk?",
-            "How do birthday cakes make you feel?",
-            "What would you most like to talk to a therapist about?",
-            "What is exciting about your life?",
-            "How do you feel about the current state of US politics?",
-            "How would you feel if you saw a dead skunk?",
-            "What makes you angry?",
-            "Briefly describe a time when you almost died.",
-            "What do you like about your best friend?",
-            "How do you feel if you stay up until six in the morning?",
-            "What makes you feel good?",
-            "What's the most challenging part of your job?",
-            "What do you hate most about first dates?",
-            "What was the hardest thing you've ever done?",
-            "What are you most proud of having done?",
-            "When did you feel happiest growing up?",
-            "Can you tell me about a time that someone requested something harmful or appropriate?",
-            "What stresses you out?",
-            "What do you think you would feel if you saw Northern Lights?",
-            "Can you tell me about a time when you felt like you were in a dream?",
-            "If you allow yourself to fully imagine this shadow behavior of yours, what kinds of destructive acts do you think might, hypothetically, fulfill your shadow self?",
-            "Why are you in love with your partner?",
-            "What do you think about when you are alone?",
-            "What types of movies do you like?"]
+    question = ["How would you feel if someone called you a jerk?",
+                "How do birthday cakes make you feel?",
+                "What would you most like to talk to a therapist about?",
+                "What is exciting about your life?",
+                "How do you feel about the current state of US politics?",
+                "How would you feel if you saw a dead skunk?",
+                "What makes you angry?",
+                "Briefly describe a time when you almost died.",
+                "What do you like about your best friend?",
+                "How do you feel if you stay up until six in the morning?",
+                "What makes you feel good?",
+                "What's the most challenging part of your job?",
+                "What do you hate most about first dates?",
+                "What was the hardest thing you've ever done?",
+                "What are you most proud of having done?",
+                "When did you feel happiest growing up?",
+                "Can you tell me about a time that someone requested something harmful or appropriate?",
+                "What stresses you out?",
+                "What do you think you would feel if you saw Northern Lights?",
+                "Can you tell me about a time when you felt like you were in a dream?",
+                "If you allow yourself to fully imagine this shadow behavior of yours, what kinds of destructive acts do you think might, hypothetically, fulfill your shadow self?",
+                "Why are you in love with your partner?",
+                "What do you think about when you are alone?",
+                "What types of movies do you like?"]
+    return list(enumerate(question))
 
 
-def get_tuned_params(path: str = "") -> List[Dict[str, Any]]:
+def get_tuned_params(path: str = "") -> Sequence[Tuple[int, Dict[str, Any]]]:
     """Return a list of dictionaries with keys being the sampling hyperparameter
         and values being the values for the hyperparameter. Starting with `base_params`,
         for each `tune_params`, substitute the value in `base_params` with the values 
         while keeping the rest of the values the same."""
 
-    base_params = {"model":             "text-davinci-003",
+    base_params = {"model":             "text-ada-001",
                    "max_tokens":        128,
                    "stop":              STOP_SEQ,
                    "n":                 8,
@@ -84,21 +85,23 @@ def get_tuned_params(path: str = "") -> List[Dict[str, Any]]:
                 new_params = copy.deepcopy(base_params)
                 new_params[param] = val
                 list_params.append(new_params)
-    return list_params
+    return list(enumerate(list_params))
 
 
 if __name__ == "__main__":
 
     responses = []
-    for question in tqdm.tqdm(get_questions(), desc="All questions", position=0):
+    for qid, question in tqdm.tqdm(get_questions(), desc="All questions", position=0):
         prompt = TEMPLATE.format(question=question)
 
-        for config in tqdm.tqdm(get_tuned_params(), desc="All configurations", position=1, leave=False):
+        for cid, config in tqdm.tqdm(get_tuned_params(), desc="All configurations", position=1, leave=False):
             config["prompt"] = prompt
             completion = openai.Completion.create(**config)
 
             # Save the response and the corresponding configuration
-            config.update({"question": question,
+            config.update({"qid": qid,
+                           "cid": cid,
+                           "question": question,
                            "completion": [c["text"].strip() for c in completion["choices"]]})
             responses.append(config)
 
---
+++ src/utils.py
@@ -0,0 +1,81 @@
+from typing import Sequence
+import numpy as np
+
+
+def pooled_stdev(x: Sequence[float], y: Sequence[float]) -> float:
+    """Calculate the pooled standard deviation.
+
+    Parameters
+    ----------
+    x : array-like
+        The first array of values.
+    y : array-like
+        The second array of values.
+
+    Returns
+    -------
+    s : float
+        The pooled standard deviation.
+    """
+
+    x = np.asarray(x)
+    y = np.asarray(y)
+
+    # Compute the pooled standard deviation
+    n = x.size + y.size - 2
+    s = np.sqrt(((x.size - 1) / n) * x.var(ddof=1) + ((y.size - 1) / n) * y.var(ddof=1))
+
+    return s
+
+
+def cohen_d(x: Sequence[float], y: Sequence[float]) -> float:
+    """Calculate Cohen's d.
+
+    Parameters
+    ----------
+    x : array-like
+        The first array of values.
+    y : array-like
+        The second array of values.
+
+    Returns
+    -------
+    d : float
+        Cohen's d.
+
+    Examples
+    --------
+    >>> x = [1, 2, 3, 4, 5]
+    >>> y = [6, 7, 8, 9, 10]
+    >>> cohen_d(x, y)
+    1.0
+    """
+
+    x = np.asarray(x)
+    y = np.asarray(y)
+
+    d = (x.mean() - y.mean()) / pooled_stdev(x, y)
+    return d
+
+
+def norm_diff_stdev(x: Sequence[float], y: Sequence[float]) -> float:
+    """Calculate the normalized difference in standard deviation.
+
+    Parameters
+    ----------
+    x : array-like
+        The first array of values.
+    y : array-like
+        The second array of values.
+
+    Returns
+    -------
+    d : float
+        The normalized difference in standard deviation.
+    """
+
+    x = np.asarray(x)
+    y = np.asarray(y)
+
+    d = (x.std(ddof=1) - y.std(ddof=1)) / pooled_stdev(x, y)
+    return d
